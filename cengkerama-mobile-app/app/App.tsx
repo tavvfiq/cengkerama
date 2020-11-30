c76d73cd8ec4e3ede4d5accc7b7435b74e31a5a6
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-11-30 12:06:01 +0700

    create repo and mobile app boilerplate

---
+++ cengkerama-mobile-app/app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {}
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
