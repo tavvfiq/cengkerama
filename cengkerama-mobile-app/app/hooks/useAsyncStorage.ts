9aa0b28fe63ef85fb0f21437290d6ec238250043
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-12-11 09:33:27 +0700

    [Taufiq] Add @reac-native-async-storage

---
+++ cengkerama-mobile-app/app/hooks/useAsyncStorage.ts
@@ -0,0 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+export default function useAsyncStorage<T>(
+  key: string,
+  nullValue: T,
+): [T, Dispatch<SetStateAction<T>>, () => Promise<void>, () => Promise<void>] {
+  const [value, setValue] = useState<T>(nullValue);
+
+  const readFromAsyncStorage = async () => {
+    try {
+      const payload = await AsyncStorage.getItem(key);
+      console.log("READ_ASYNC");
+      if (payload !== null) {
+        setValue(JSON.parse(payload));
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    readFromAsyncStorage();
+  }, []);
+
+  const writeToAsyncStorage = async () => {
+    try {
+      const payload = JSON.stringify(value);
+      await AsyncStorage.setItem(key, payload);
+      console.log("WRITE_ASYNC");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const clearStorage = async () => {
+    try {
+      await AsyncStorage.removeItem(key);
+      setValue(nullValue);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return [value, setValue, writeToAsyncStorage, clearStorage];
+}
