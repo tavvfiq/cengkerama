9117aee9881f1e9154e1d374ada1ccc6c7b937f6
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-12-11 09:34:21 +0700

    [Taufiq] Move firestore related stuff to useMessage

--- cengkerama-mobile-app/app/hooks/useMessage.ts
+++ cengkerama-mobile-app/app/hooks/useMessage.ts
@@ -1,29 +1,120 @@
-import { useState, useEffect } from "react";
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+import { useEffect, Dispatch, SetStateAction, useState } from "react";
 
-import { MessageProps } from "../interface";
+import { MessageProps, RoomProps } from "../interface";
 import { FirestoreService } from "../services";
 
-const useMessage = (roomId: string) => {
-  const [message, setMessage] = useState<MessageProps[]>([]);
+import useAsyncStorage from "./useAsyncStorage";
+
+const useMessage = (
+  roomId: string,
+  userId: string,
+): [MessageProps[] | null, Dispatch<SetStateAction<MessageProps[]>>] => {
+  const [message, setMessage, writeToStorage] = useAsyncStorage<MessageProps[]>(
+    roomId + userId,
+    [],
+  );
+
+  const [snapshot, setSnapshot] = useState<
+    FirebaseFirestoreTypes.QuerySnapshot<FirebaseFirestoreTypes.DocumentData>
+  >();
+
+  const truncateMessage = () => {
+    if (snapshot?.docChanges()) {
+      const snapshotLength = snapshot.docChanges().length;
+      const messageLength = message.length;
+      if (
+        snapshotLength > 0 &&
+        (snapshotLength > messageLength || messageLength === 0)
+      ) {
+        const newMessage: MessageProps[] = snapshot?.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        });
+        setMessage(newMessage);
+      } else if (snapshotLength > 0 && snapshotLength < messageLength) {
+        console.log("APPEND_FROM_SNAPSHOT");
+        const newMessage: MessageProps[] = [...message];
+        snapshot.docChanges().map((item) => {
+          const idx = message.findIndex((m) => {
+            return item.doc.id === m.id;
+          });
+          if (idx >= 0) {
+            newMessage.splice(idx, 1, {
+              id: item.doc.id,
+              ...item.doc.data(),
+            });
+          } else {
+            newMessage.push({ id: item.doc.id, ...item.doc.data() });
+          }
+        });
+        setMessage(newMessage);
+      }
+    }
+  };
 
   useEffect(() => {
-    const subscriber = FirestoreService.MessageCollection.doc(roomId)
-      .collection("messages")
+    const subscriber = FirestoreService.MessageCollection(roomId)
       .orderBy("sentAt", "asc")
-      .onSnapshot((result) => {
-        if (result) {
-          const messages = result.docs.map((doc) => {
-            return { ...doc.data(), id: doc.id };
-          });
-          setMessage(() => messages);
-        }
-      });
+      .onSnapshot(
+        (result) => {
+          if (result) {
+            setSnapshot(result);
+          }
+        },
+        (error) => console.error(error),
+      );
     // Unsubscribe from events when no longer in use
     return () => {
+      writeToStorage();
       subscriber();
     };
-  }, [roomId]);
-  return message;
+  }, [roomId, userId]);
+
+  useEffect(() => {
+    truncateMessage();
+  }, [snapshot]);
+
+  // mark as read by current user
+  useEffect(() => {
+    FirestoreService.RoomCollection.doc(roomId)
+      .get()
+      .then((snapshotQuery) => {
+        const { recentMessage } = snapshotQuery.data() as RoomProps;
+        const readBy = recentMessage?.readBy;
+        const isRead = readBy?.indexOf(userId);
+        if ((isRead as number) < 0) {
+          readBy?.push(userId);
+          // IIFE for updating recent readBy
+          (async () => {
+            try {
+              await FirestoreService.RoomCollection.doc(roomId).update({
+                "recentMessage.readBy": readBy,
+              });
+            } catch (error) {
+              console.error(error);
+            }
+          })();
+        }
+      });
+  }, []);
+
+  // update the recent message
+  useEffect(() => {
+    if (message.length > 0) {
+      const newRecentMessage = {
+        messageText: message[message?.length - 1].messageText,
+        sentAt: message[message?.length - 1].sentAt,
+        sentBy: userId,
+        readBy: [userId],
+        type: message[message?.length - 1].type,
+      };
+      FirestoreService.RoomCollection.doc(roomId).update({
+        recentMessage: newRecentMessage,
+      });
+    }
+  }, [message.length]);
+
+  return [message, setMessage];
 };
 
 export default useMessage;
--- cengkerama-mobile-app/app/services/FirestoreService.ts
+++ cengkerama-mobile-app/app/services/FirestoreService.ts
@@ -1,5 +1,77 @@
-import firestore from '@react-native-firebase/firestore';
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
 
-export const MessageCollection = firestore().collection('message');
+export const MessageCollection = (roomId: string) =>
+  firestore().collection("message").doc(roomId).collection("messages");
 
-export const RoomCollection = firestore().collection('room');
+export const RoomCollection = firestore().collection("room");
+
+export type UploadConfig = {
+  storagePath: string; // in firebase storage
+  filePath: string; // file uri
+};
+
+export const Storage = {
+  uploadFile: async (uploadConfig: UploadConfig) => {
+    try {
+      const uploadTask = storage()
+        .ref(uploadConfig.storagePath)
+        .putFile(uploadConfig.filePath);
+      await uploadTask;
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  },
+  uploadFiles: async (uploadConfig: UploadConfig[]) => {
+    try {
+      for (let idx = 0; idx < uploadConfig.length; idx++) {
+        const uploadTask = storage()
+          .ref(uploadConfig[idx].storagePath)
+          .putFile(uploadConfig[idx].filePath);
+        await uploadTask;
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  },
+  uploadFileAndGetLink: async (
+    uploadConfig: UploadConfig,
+    container: { [key: string]: string },
+  ) => {
+    try {
+      const uploadTask = storage()
+        .ref(uploadConfig.storagePath)
+        .putFile(uploadConfig.filePath);
+      await uploadTask;
+      const url = await storage()
+        .ref(uploadConfig.storagePath)
+        .getDownloadURL();
+      container.url = url;
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  },
+  uploadFilesAndGetLink: async (
+    uploadConfig: UploadConfig[],
+    container: { [key: string]: string }[],
+  ) => {
+    try {
+      for (let idx = 0; idx < uploadConfig.length; idx++) {
+        const uploadTask = storage()
+          .ref(uploadConfig[idx].storagePath)
+          .putFile(uploadConfig[idx].filePath);
+        await uploadTask;
+        const url = await storage()
+          .ref(uploadConfig[idx].storagePath)
+          .getDownloadURL();
+        container.push({ url: url });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  },
+};
