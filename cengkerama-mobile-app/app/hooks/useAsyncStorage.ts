107910d51b6c8ab66203532482c76954439f0823
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-12-16 21:26:45 +0700

    [Taufiq] update asyncstorage hooks to use wrapper

--- cengkerama-mobile-app/app/hooks/useAsyncStorage.ts
+++ cengkerama-mobile-app/app/hooks/useAsyncStorage.ts
@@ -1,20 +1,29 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+import StorageWrapper from "../utils/asyncStorageWrapper";
 
 export default function useAsyncStorage<T>(
   key: string,
   nullValue: T,
-): [T, Dispatch<SetStateAction<T>>, () => Promise<void>, () => Promise<void>] {
+): [
+  T,
+  Dispatch<SetStateAction<T>>,
+  () => Promise<void>,
+  () => Promise<void>,
+  () => Promise<void>,
+] {
   const [value, setValue] = useState<T>(nullValue);
 
   const readFromAsyncStorage = async () => {
     try {
-      const payload = await AsyncStorage.getItem(key);
+      const payload = await StorageWrapper.read(key);
       if (__DEV__) {
         console.log("READ_ASYNC");
       }
       if (payload !== null) {
         setValue(JSON.parse(payload));
+      } else {
+        await StorageWrapper.write(key, JSON.stringify(nullValue));
       }
     } catch (e) {
       console.error(e);
@@ -22,13 +31,15 @@ export default function useAsyncStorage<T>(
   };
 
   useEffect(() => {
-    readFromAsyncStorage();
+    (async () => {
+      await readFromAsyncStorage();
+    })();
   }, []);
 
   const writeToAsyncStorage = async () => {
     try {
       const payload = JSON.stringify(value);
-      await AsyncStorage.setItem(key, payload);
+      await StorageWrapper.write(key, payload);
       if (__DEV__) {
         console.log("WRITE_ASYNC");
       }
@@ -39,12 +50,18 @@ export default function useAsyncStorage<T>(
 
   const clearStorage = async () => {
     try {
-      await AsyncStorage.removeItem(key);
+      await StorageWrapper.clear(key);
       setValue(nullValue);
     } catch (e) {
       console.error(e);
     }
   };
 
-  return [value, setValue, writeToAsyncStorage, clearStorage];
+  return [
+    value,
+    setValue,
+    writeToAsyncStorage,
+    readFromAsyncStorage,
+    clearStorage,
+  ];
 }
